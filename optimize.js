1409dab0b8806dd085ea227ba7a28a8ff2f4c952
Barry <barrycumbie@gmail.com>
2023-04-20 14:05:55 -0500

    ????

--- optimize.js
+++ optimize.js
@@ -1,6 +1,10 @@
+
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2J1cmZvcmQxMTIxIiwiYSI6ImNsZzRqcXRjeDBpN28zZnIzNzFwc3hwOWIifQ.E6KooNfwrwsKBkhWpN9CGQ';
 const addAddressButton = document.getElementById("addAddress");
 const optimizeRouteButton = document.getElementById("optimizeRoute");
+
+
 
 let routeDetails = 
 {
@@ -12,6 +16,8 @@ toBeChecked : [true, true],
 predecesor : []
 };
 localStorage.clear();
+
+// console.log(routeDetails.start, routeDetails.end)
 
 addAddressButton.addEventListener("click", (event) => 
 {
@@ -34,17 +40,22 @@ optimizeRouteButton.addEventListener("click", (event) =>
 	//	optimizeRoute(routeDetails);
 });
 
-async function getCoord(coordObj, place)
+//take route strings into coordinates
+//todo: do we need async here? or not 
+ async function getCoord(coordObj, place)
 {
 	const apiURL = `https://nominatim.openstreetmap.org/search?q=${place}&format=geojson`;
 	const query = await fetch(apiURL);
+	console.log("query: ", query);
 	const json = await query.json();
+	console.log("query json: ", json);
 	console.dir(json.features[0].geometry.coordinates);
 	console.dir(json.features[0].geometry.coordinates[0]);
 	coordObj.latitude = json.features[0].geometry.coordinates[0];
 	coordObj.longitude = json.features[0].geometry.coordinates[1];
 	console.dir(coordObj);
 	console.dir(coordObj.latitude);
+	return coordObj; 
 }
 
 // create a function to make a directions request
@@ -54,14 +65,26 @@ routeObject.distMatrix = new Array(routeObject.addressList.length);
 
 for(let i = 0; i < routeObject.addressList.length; i++)
 {
+	console.log("loopin' : ", i)
 	let startCoord = 
 	{
-		latitude : undefined,
+		latitude :  undefined,
 		longitude : undefined
 	}
-	getCoord(startCoord, routeObject.addressList[i]);
-	console.log("Start Coord");
-	console.dir(startCoord);
+	
+	let coordResult = await getCoord(startCoord, routeObject.addressList[i]).then(
+		(result) => {
+
+			console.log("result: ", result);
+		console.log("get coord: ", startCoord);
+		console.log("get coord: ", routeObject.addressList[i]);
+		console.log("get coord result:", getCoord(startCoord, routeObject.addressList[i]));
+		console.log("Start Coord");
+		console.dir(startCoord);
+	}
+
+	)
+	
 	for(let j = 1; j < routeObject.addressList.length; j++)
 	{
 		let endCoord = 
@@ -74,9 +97,11 @@ for(let i = 0; i < routeObject.addressList.length; i++)
 		console.dir(endCoord.latitude);
 		const query = await fetch
 		(
-			`https://api.mapbox.com/directions/v5/mapbox/driving/${startCoord.latitude.PromiseResult},${startCoord.longitude.PromiseResult};${endCoord.latitude},${endCoord.longitude}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+			`https://api.mapbox.com/directions/v5/mapbox/driving/${startCoord.latitude},${startCoord.longitude};${endCoord.latitude},${endCoord.longitude}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
 			{ method: 'GET' }
 		);
+		console.log("passing get:", `https://api.mapbox.com/directions/v5/mapbox/driving/${startCoord.latitude},${startCoord.longitude};${endCoord.latitude},${endCoord.longitude}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`);
+
 		const json = await query.json();
 		const data = json.routes[0];
 		const steps = data.legs[0].steps;
@@ -84,6 +109,7 @@ for(let i = 0; i < routeObject.addressList.length; i++)
 		routeObject.distMatrix[j][i] = Math.floor(data.duration / 60);
 	}
 }
+console.log()
 }
 
 //function optimizeRoute(routeObject)
